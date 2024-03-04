126b0036479adc21a3a3a2cb6557a14cca0fea40
Jenna Cioffi <JMCioffi4@gmail.com>
2024-03-04 16:35:51 -0700

    creating card component

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -5,6 +5,9 @@ export default function Card() {
   return (
     <div style={styles.cardContainer}>
         <div style={styles.outerCard}>
+          <div style={styles.innerRectangle}>
+            This is the inner rectangle.
+          </div>
             This is a card component.
         </div>
     </div>
