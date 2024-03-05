490a5018658c26953e0a8784649a9aefc7e2ce44
Jenna Cioffi <JMCioffi4@gmail.com>
2024-03-04 23:34:14 -0700

    saving progress on card component work. Text is centered in card. TODO: get bullet points to left side

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -3,13 +3,22 @@ import styles from "./card.styles.js"; // Import your CSS styles
 
 export default function Card() {
   return (
-    <div style={styles.cardContainer}>
+    <div style={styles.mainContainer}>
+      <div style={styles.cardBackgroundContainer}>
         <div style={styles.outerCard}>
           <div style={styles.innerRectangle}>
-            This is the inner rectangle.
+            {/* This is the inner rectangle. */}
           </div>
-            This is a card component.
         </div>
+      </div>
+      <div style={styles.bulletPointTitleContainer}>
+        <div style={styles.bulletPointTitle}> Skills </div>
+      </div>
+      <div style={styles.bulletPointItemsContainer}>
+        <div style={styles.bulletPointItem}>- 1st Bullet Point Item</div>
+        <div style={styles.bulletPointItem}>- 2nd Bullet Point Item</div>
+        <div style={styles.bulletPointItem}>- 3rd Bullet Point Item</div>
+      </div>
     </div>
   );
 };
--- src/components/Card/card.styles.js
+++ src/components/Card/card.styles.js
@@ -1,30 +1,50 @@
 const styles = {
-  cardContainer: {
-    alignItems: "center",
+  mainContainer: { // parent to all children below
     display: "flex",
     justifyContent: "center",
+    padding: '20px',
+  },
+  cardBackgroundContainer: {
+    display: "flex", // makes whole card compress with browser window movement
+    maxWidth: "100%", // ^
   },
   outerCard: {
     alignItems: "center",
-    backgroundColor: "blue", 
+    backgroundColor: "blue",
     borderRadius: "8px",
-    color: "white", // Text Color
     display: "flex",
-    height: "250px",
+    height: "300px",
     justifyContent: "center",
     position: "relative",
-    width: "450px",
+    width: "850px",
   },
   innerRectangle: {
-    alignItems: "center",
     border: "2px dashed #FFFFFF",
     borderRadius: "6px",
     color: "red",
-    height: "10%",
-    padding: "22%",
+    height: "85%",
+    padding: "1%",
     position: "absolute",
     textAlign: "center",
-    width: "50%",
+    width: "90%",
+  },
+  bulletPointTitleContainer: {
+    display: "flex",
+    margin: "25px", // Add some margin for spacing
+    position: "absolute",
+  },
+  bulletPointTitle: {
+    alignContent: "center",
+    color: "black",
+  },
+  bulletPointItemsContainer: { 
+    justifyContent: 'flex-start' ,
+    margin: '50px', 
+    position: "absolute", 
+  },
+  bulletPointItem: {
+    alignContent: "flex-start",
+    color: "black",
   },
 };
 
