50c87c03ada2d4aa9ff6caf8055a7cfa6285596f
Jenna Cioffi <59583650+jennacioffi@users.noreply.github.com>
2024-03-05 17:44:59 -0700

    Card Component Creation + ESLint (#4)
    Created Card Component & Added ESLint

---
+++ src/components/Card/card.styles.js
@@ -0,0 +1,57 @@
+const styles = {
+  mainContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '20px',
+  },
+  cardBackgroundContainer: {
+    display: 'flex',
+    maxWidth: '100%',
+    position: 'relative',
+  },
+  outerCard: {
+    alignItems: 'center',
+    backgroundColor: '#6E8DFF',
+    borderRadius: '8px',
+    display: 'flex',
+    height: '300px',
+    justifyContent: 'center',
+    position: 'relative',
+    width: '850px',
+  },
+  innerRectangle: {
+    border: '2px dashed #FFFFFF',
+    borderRadius: '6px',
+    color: 'red',
+    height: '85%',
+    padding: '1%',
+    position: 'absolute',
+    textAlign: 'center',
+    width: '90%',
+  },
+  contentContainer: {
+    left: '0',
+    position: 'absolute',
+    top: '50%',
+    transform: 'translateY(-160%)',
+    width: '100%',
+  },
+  bulletPointTitleContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  bulletPointTitle: {
+    color: 'black',
+  },
+  bulletPointItemsContainer: {
+    alignItems: 'flex-start',
+    display: 'flex',
+    flexDirection: 'column',
+    marginLeft: '50px',
+  },
+  bulletPointItem: {
+    color: 'black',
+  },
+};
+
+export default styles
