896990b4e21e904cc7167f832d656bc3e4ce1525
Jenna Cioffi <JMCioffi4@gmail.com>
2024-03-27 11:59:31 -0600

    working more on contact me card

--- src/components/Card/ContactMeCard/contact_me_card.js
+++ src/components/Card/ContactMeCard/contact_me_card.js
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './contact_me_card.styles';
 
-export default function ContactMeCard({}) {
+export default function ContactMeCard() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
 
   return (
     <div style={styles.mainContainer}>
       <div style={styles.outerCard}>
-        {/* // Put Stuff here */}
-        Test Words
+        <div style={styles.cardHeader}>
+          Contact Me
+        </div>
+        <div style={styles.input}>
+          <label htmlFor="name">Your Name:</label><br />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            style={styles.inputField}
+          />
+        </div>
+        <div style={styles.input}>
+          <label htmlFor="email">Your Email:</label><br />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            style={styles.inputField}
+          />
+        </div>
+        <div style={styles.input}>
+          <label htmlFor="message">Your Message:</label><br />
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            rows="4"
+            cols="50"
+            style={styles.textareaField}
+          ></textarea>
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/Card/ContactMeCard/contact_me_card.styles.js
+++ src/components/Card/ContactMeCard/contact_me_card.styles.js
@@ -1,16 +1,53 @@
+import { fonts } from '../../../styles/fonts.js';
+
 const styles = {
   mainContainer: {
-    backgroundColor: 'red',
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     padding: '20px',
-    width: '50%',
+    paddingLeft: '11.5%',
   },
   outerCard: {
+    alignContent: 'flex-start',
+    backgroundColor: 'red',
     borderRadius: '10px',
-    maxWidth: '100%',
     paddingVertical: '50px',
+    width: '50%',
+    height: '320px'
   },
-}
+  cardHeader: {
+    display: 'flex',
+    fontFamily: fonts.cardTitle,
+    justifyContent: 'center',
+    fontSize: '30px',
+    marginBottom: '20px',
+  },
+  input: {
+    marginBottom: '20px', 
+    marginLeft: '5%',
+    marginRight: '5%'
+  },
+  inputField: {
+    display: 'flex',
+    width: '90%',
+    padding: '2%',
+    borderRadius: '5px',
+    border: '1px solid #ccc',
+    boxSizing: 'border-box',
+    marginTop: '5px',
+    // marginLeft: '5%',
+    // marginRight: '5%'
+  },
+  textareaField: {
+    width: '90%',
+    padding: '8px',
+    borderRadius: '5px',
+    border: '1px solid #ccc',
+    boxSizing: 'border-box',
+    marginTop: '5px',
+    // marginRight: '5%',
+    // marginLeft: '5%',
+  },
+};
 
-export default styles
+export default styles;
