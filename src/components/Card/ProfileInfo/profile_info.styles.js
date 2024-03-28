2bf7dbea8128e8579fc74cd9abcf2b01095d0fe3
Jenna Cioffi <JMCioffi4@gmail.com>
2024-03-28 12:06:10 -0600

    ^

--- src/components/Card/ProfileInfo/profile_info.styles.js
+++ src/components/Card/ProfileInfo/profile_info.styles.js
@@ -14,20 +14,19 @@ const styles = {
   circle: {
     width: '250px', 
     height: '250px', 
-    borderRadius: '100%', 
+    // borderRadius: '50%', 
     overflow: 'hidden',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     border: '10px solid transparent', 
-    borderImage: 'linear-gradient(to right, #ff7e5f, #feb47b)',
-    borderImageSlice: 1
+    borderImage: `linear-gradient(to bottom, ${colors.white}, ${colors.azureBlue})`,
+    borderImageSlice: 1,
   },
   image: {
     width: '100%', 
     height: '100%',
     objectFit: 'cover',
-    borderRadius: '50%', 
   },
   text: {
     marginTop: '10px',
