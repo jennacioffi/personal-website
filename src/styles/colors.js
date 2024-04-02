9a9d3949e1f5323b6783cffb50db77697871b9b1
Jenna Cioffi <JMCioffi4@gmail.com>
2024-04-01 22:32:59 -0600

    cleanup colors.js

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -8,12 +8,5 @@ export const colors = {
   'darkerBlue': '#354DFF',
   'intermediateBlue': '#274DDA',
   'darkerIntermediateBlue': '#0A2F99',
-  'lightGreen': '#2C9E00',
-  'green1': '#1B8400',
-  'green2': '#0A6A00',
-  'green3': '#005000',
-  'green4': '#004366',
-  'green5': '#003C4D',
-  'green6': '#003538',
   'black': '#000000'
 };
