43c7d82d2a09e0478488b9705e9a3c3915e9bbde
Jenna Cioffi <JMCioffi4@gmail.com>
2024-04-27 15:50:32 -0600

    New Design
    * Reset project to setup new design

--- src/App.styles.js
+++ src/App.styles.js
@@ -1,37 +1,9 @@
-import { colors } from './styles/colors.js'
+// import { colors } from './styles/colors.js'
 
 const styles = {
   main: {
-    background: `linear-gradient(${colors.websiteBGGradient1}, ${colors.websiteBGGradient2}, ${colors.websiteBGGradient3})`,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    alignContent: 'center',
-    padding: '4%',
-    minWidth: '600px',
-    maxWidth: '1000px',
-    width: '100%',
-  },
-  multiCards: {
-    overflow: 'auto',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignContent: 'space-between',
-    minWidth: '50%',
-
-  },
-  leftCards: {
-    flex: 2,
-  },
-  rightCards: {
-    minWidth: '45%',
-    width: '100%', 
-    display: 'flex', 
-    flexDirection: 'column', 
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-  },
+    backgroundColor: 'red',
+  }
 };
 
 export default styles;
--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,5 +1,45 @@
+import '@openfonts/kurale_all';
+
 export const fonts = {
-  cardBulletPointItems: 'serif',
-  cardTitle: 'serif',
-  contactMeInputHeaders: 'serif',
+  kuraleFont: 'Kurale, serif',
 };
+
+export const PageTitleTextWhite = {
+  font: fonts.kuraleFont,
+  fontSize: 96,
+}
+
+export const DropDownMenuItem = {
+  font: fonts.kuraleFont,
+  fontSize: 26,
+}
+
+export const ExperienceH1 = {
+  font: fonts.kuraleFont,
+  fontSize: 40,
+}
+
+export const ExperienceH2 = {
+  font: fonts.kuraleFont,
+  fontSize: 34,
+}
+
+export const ExperienceH3 = {
+  font: fonts.kuraleFont,
+  fontSize: 30,
+}
+
+export const ExperienceH4 = {
+  font: fonts.kuraleFont,
+  fontSize: 34,
+}
+
+export const NavBarItemsText = {
+  font: fonts.kuraleFont,
+  fontSize: 40,
+}
+
+export const ProjectSkillItem = {
+  font: fonts.kuraleFont,
+  fontSize: 40,
+}
