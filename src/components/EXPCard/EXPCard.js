404872e31c5d2613638b4401548c555138f8c6d4
Jenna Cioffi <JMCioffi4@gmail.com>
2024-05-15 12:03:17 -0600

    Update EXPCard.js

--- src/components/EXPCard/EXPCard.js
+++ src/components/EXPCard/EXPCard.js
@@ -24,69 +24,104 @@ const EXPSkilItem = ({ skillItem }) => {
   )
 }
 
+const JobSummary = ({ EXPItem, isMobile }) => {
+  return (
+    <>
+      <div style={{ ...(isMobile && { marginBottom: '15px', }), ...styles.SummaryContainer}}>
+        <div style={styles.SummaryText}>
+          {EXPItem.expSummary && (
+            <>
+              {EXPItem.expSummary.briefSummary && (
+                <div>{EXPItem.expSummary.briefSummary}</div>
+              )}
+              {EXPItem.expSummary.bulletPoints && (
+                <ul>
+                  {EXPItem.expSummary.bulletPoints.map((point, index) => (
+                    <li key={index} style={styles.SummaryBulletPoints}>{point}</li>
+                  ))}
+                </ul>
+              )}
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  )
+}
+
+const JobInfo = ({ EXPItem }) => {
+  return (
+    <>
+      <div style={styles.EXPJobInfoContainer}>
+        <div style={styles.expTitle}>
+          {EXPItem.expTitle}
+        </div>
+        <div style={styles.expCompanyText}>
+          {EXPItem.expCompanyLINK ? (
+            <a 
+              href={EXPItem.expCompanyLINK} 
+              target="_blank" 
+              rel="noopener noreferrer" 
+            >
+              {EXPItem.expCompany}
+            </a>
+          ) : (
+            <div>{EXPItem.expCompany}</div>
+          )}
+        </div>
+        <div style={styles.expTimeWorked}>
+          {EXPItem.expTimeWorked}
+        </div>
+      </div>
+    </>
+  )
+}
+
+const SkillsInfo = ({ EXPItem, isMobile }) => {
+  return (
+    <>
+      <div style={styles.EXPSkillsContainer}>
+        <div style={{...styles.EXPSkillsBox, ...(isMobile && { width: '375px', marginBottom: '15px' })}}>
+          {EXPItem.skillList.map((skillItem, index) => (
+            <EXPSkilItem key={index} isMobile={isMobile} skillItem={skillItem}/>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
 
 const EXPCard = ({ isMobile, EXPItem }) => {
-  const handleCompanyLinkClick = (event) => {
-    event.stopPropagation(); 
-  };
 
   return (
     <>
-      <div style={{...styles.outerContainer, ...(isMobile && { height: '900px', width: '425px'})}}>
-        <div style={{...styles.cardContainer, ...(isMobile && { flexDirection: 'column' })}}>
-          <div style={{...styles.SummaryOuterContainer, ...(isMobile && { width: '375px' })}}>
-            <div style={styles.SummaryContainer}>
-              <div style={styles.SummaryText}>
-                {EXPItem.expSummary && (
-                  <>
-                    {EXPItem.expSummary.briefSummary && (
-                      <div>{EXPItem.expSummary.briefSummary}</div>
-                    )}
-                    {EXPItem.expSummary.bulletPoints && (
-                      <ul>
-                        {EXPItem.expSummary.bulletPoints.map((point, index) => (
-                          <li key={index} style={styles.SummaryBulletPoints}>{point}</li>
-                        ))}
-                      </ul>
-                    )}
-                  </>
-                )}
-              </div>
+      {isMobile ? (
+        <div style={{...styles.outerContainer, ...(isMobile && { height: '900px', width: '425px'})}}>
+          <div style={{...styles.cardContainer, ...(isMobile && { flexDirection: 'column' })}}>
+            <div style={{...styles.SummaryOuterContainer, ...(isMobile && { width: '375px' })}}>
+              <JobInfo EXPItem={EXPItem}/>
+              
             </div>
-          </div>
-          <div style={styles.TitleAndSkillsContainer}>
-            <div style={styles.EXPJobInfoContainer}>
-              <div style={styles.expTitle}>
-                {EXPItem.expTitle}
-              </div>
-              <div style={styles.expCompanyText}>
-                {EXPItem.expCompanyLINK ? (
-                  <a 
-                    href={EXPItem.expCompanyLINK} 
-                    target="_blank" 
-                    rel="noopener noreferrer" 
-                    onClick={handleCompanyLinkClick}
-                  >
-                    {EXPItem.expCompany}
-                  </a>
-                ) : (
-                  <div>{EXPItem.expCompany}</div>
-                )}
-              </div>
-              <div style={styles.expTimeWorked}>
-                {EXPItem.expTimeWorked}
-              </div>
-            </div>
-            <div style={styles.EXPSkillsContainer}>
-              <div style={{...styles.EXPSkillsBox, ...(isMobile && { width: '375px' })}}>
-                {EXPItem.skillList.map((skillItem, index) => (
-                  <EXPSkilItem key={index} isMobile={isMobile} skillItem={skillItem}/>
-                ))}
-              </div>
+            <div style={styles.TitleAndSkillsContainer}>
+              <JobSummary EXPItem={EXPItem} isMobile={isMobile}/>
+              <SkillsInfo EXPItem={EXPItem} isMobile={isMobile}/>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div style={{...styles.outerContainer, ...(isMobile && { height: '900px', width: '425px'})}}>
+          <div style={{...styles.cardContainer, ...(isMobile && { flexDirection: 'column' })}}>
+            <div style={{...styles.SummaryOuterContainer, ...(isMobile && { width: '375px' })}}>
+              <JobSummary EXPItem={EXPItem}/>
+            </div>
+            <div style={styles.TitleAndSkillsContainer}>
+              <JobInfo EXPItem={EXPItem}/>
+              <SkillsInfo EXPItem={EXPItem} isMobile={isMobile}/>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   )
 }
